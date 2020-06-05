29fbd9f3f980de80ba95977c30fb11714a19715d
SongNingning <qfsnn1117@gmail.com>
2020-06-05 09:28:50 +0800

    29.顺时针打印矩阵 solved

--- 29-顺时针打印矩阵/src/Solution1.java
+++ 29-顺时针打印矩阵/src/Solution1.java
@@ -3,15 +3,44 @@ import java.util.Queue;
 
 /**
  * 面试题29. 顺时针打印矩阵
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
  *
- * https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+ * 示例 1：
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ *
+ * 示例 2：
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * 限制：
+ * 0 <= matrix.length <= 100
+ * 0 <= matrix[i].length <= 100
+ *
+ * 注意：本题与力扣 54 题相同：https://leetcode-cn.com/problems/spiral-matrix/
  *
  * @Author: Song Ningning
  * @Date: 2020-05-12 12:45
  */
 public class Solution1 {
 
-    int[] res;
+    /*
+     * 从外往里打印，先打印最外圈，依次打印内圈。
+     * 只剩下中间一个元素（方阵）或者一行元素（列数 > 行数）或者一列元素（行数 > 列数）时，继续输出。
+     *
+     *     ---------|   |
+     *       1   2  | 3 |
+     *     ---------|   |
+     *     | 4 | 5  | 6 |
+     *     |   |---------
+     *     | 7 | 8    9
+     *     |   |---------
+     *
+     * 每次将结果存到一个队列中，最后将从队列头部依次取出元素到返回数组中。
+     *
+     * Time：O(MN)，M 为矩阵行数，N 为矩阵列数，相当于遍历一遍。
+     * Space：O(MN)，使用了一个大小为 M * N 的队列。
+     */
     Queue<Integer> list = new LinkedList<>();
 
     public int[] spiralOrder(int[][] m) {
@@ -24,7 +53,7 @@ public class Solution1 {
         int d = m[0].length - 1;
 
         int len = m.length * m[0].length;
-        res = new int[len];
+        int[] res = new int[len];
 
         while (a <= c && b <=d) {
             printEdge(m, a++, b++, c--, d--);
@@ -70,8 +99,8 @@ public class Solution1 {
         int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
         Solution1 solution1 = new Solution1();
         int[] arr = solution1.spiralOrder(matrix);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int value : arr) {
+            System.out.print(value + " ");  // 1 2 3 6 9 8 7 4 5
         }
     }
 }
--- 29-顺时针打印矩阵/src/Solution2.java
+++ 29-顺时针打印矩阵/src/Solution2.java
@@ -10,6 +10,14 @@ import java.util.Queue;
  */
 public class Solution2 {
 
+    /*
+     * 基本思想还是先打印外圈，再打印内圈。但不再使用队列存储每次的结果，
+     * 定义一个 index，用来标识当前元素应该放在返回数组哪个位置。
+     *
+     * Time：O(MN)，M 为矩阵行数，N 为矩阵列数，相当于遍历一遍。
+     * Space：O(1)
+     */
+
     // 定义一个索引值，用于返回数组的递增添加
     int index = 0;
 
@@ -22,9 +30,7 @@ public class Solution2 {
         int c = m.length - 1;
         int d = m[0].length - 1;
 
-        int[] res;
-        int len = m.length * m[0].length;
-        res = new int[len];
+        int[] res = new int[m.length * m[0].length];
 
         while (a <= c && b <=d) {
             printEdge(m, a++, b++, c--, d--, res);
@@ -66,8 +72,8 @@ public class Solution2 {
         int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
         Solution2 solution1 = new Solution2();
         int[] arr = solution1.spiralOrder(matrix);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int value : arr) {
+            System.out.print(value + " ");  // 1 2 3 6 9 8 7 4 5
         }
     }
 }
