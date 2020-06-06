0707ffe3d926e94f0370f4bb9eee45a1884b6081
SongNingning <qfsnn1117@gmail.com>
2020-06-06 19:58:30 +0800

    28. 对称的二叉树 solved

---
+++ 28-对称的二叉树/src/Solution1.java
@@ -0,0 +1,54 @@
+/**
+ * 面试题28. 对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 限制：
+ * 0 <= 节点个数 <= 1000
+ *
+ * 注意：本题与力扣 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ *
+ * @Author: Song Ningning
+ * @Date: 2020-06-06 19:17
+ */
+public class Solution1 {
+
+    // 递归
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode node1, TreeNode node2) {
+        // 传入的两个结点均为空，返回 true
+        if (node1 == null && node2 == null)
+            return true;
+        // 如果一个为空一个不为空，返回 false
+        if (node1 == null || node2 == null)
+            return false;
+
+        // 两个结点都不为空，但是值不相等返回 false
+        if (node1.val != node2.val)
+            return false;
+        // 传入的两个结点值相等的话，再递归往下看
+        return check(node1.left, node2.right) && check(node1.right, node2.left);
+
+        // 最后的 if-else 可以简写
+        // return (node1.val == node2.val) && (check(node1.left, node2.right) && check(node1.right, node2.left));
+    }
+}
---
+++ 28-对称的二叉树/src/Solution2.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author: Song Ningning
+ * @Date: 2020-06-06 19:40
+ */
+public class Solution2 {
+
+    // 迭代
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null)
+                continue; // 注意
+            if (node1 == null || node2 == null)
+                return false;
+            if (node1.val != node2.val)
+                return false;
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
+    }
+}
