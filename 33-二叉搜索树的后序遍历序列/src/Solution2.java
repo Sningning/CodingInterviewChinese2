6adc2eb67dc5ac58d497c47995574f78d2a8b882
SongNingning <qfsnn1117@gmail.com>
2020-06-27 20:25:20 +0800

    更改换行符为 LF

---
+++ 33-二叉搜索树的后序遍历序列/src/Solution2.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: Song Ningning
+ * @Date: 2020-06-09 17:27
+ */
+public class Solution2 {
+    /**
+     * 单调栈
+     */
+    public static boolean verifyPostorder(int[] postorder) {
+        return false;
+    }
+}
