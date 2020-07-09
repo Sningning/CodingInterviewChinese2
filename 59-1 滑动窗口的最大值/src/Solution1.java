5d22346ed4c29118e97dac8229c623c3f1415d59
SongNingning <qfsnn1117@gmail.com>
2020-07-09 18:44:26 +0800

    剑指 Offer 59 - I. 滑动窗口的最大值 solved

---
+++ 59-1 滑动窗口的最大值/src/Solution1.java
@@ -0,0 +1,53 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 剑指 Offer 59 - I. 滑动窗口的最大值
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ *
+ * 示例:
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ *
+ *   滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *
+ *
+ * 提示：
+ * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+ *
+ * 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/
+ *
+ * @Author: Song Ningning
+ * @Date: 2020-07-09 8:44
+ */
+public class Solution1 {
+
+    /**
+     * 暴力
+     * 时间复杂度：O(Nk)
+     * 空间复杂度：O(1)
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            // 窗口范围内寻找最大值
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+}
