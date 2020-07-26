90df5fa8826ae3312cbcaf54ba9160934606d4a5
SongNingning <qfsnn1117@gmail.com>
2020-07-26 21:06:11 +0800

    更新代码

--- 09-2 用两个队列实现栈/src/TwoQueuesStack.java
+++ 09-2 用两个队列实现栈/src/TwoQueuesStack.java
@@ -10,10 +10,11 @@ import java.util.Queue;
 public class TwoQueuesStack {
 
     /**
-     * 思路：利用两个队列，入栈时，只向 queue 队列入队，
-     * 执行 peek 操作时：先将 queue 队列中元素逐个出队放到 help 队列中，只剩队尾元素，将剩下的元素返回，
-     *                因为是 peek 操作，还需将其再加入到 help 队列中，然后交换两个队列的角色；
-     * 执行 pop 操作时：与 peek 操作一样，只不过不需要将返回元素再压入 help 栈中
+     * 思路：利用两个队列 queue 和 helper。
+     * 入栈时，只向 queue 队列入队；
+     * 执行 peek 操作时：先将 queue 队列中元素逐个出队放到 helper 队列中，只剩队尾元素，将剩下的元素返回，
+     *                因为是 peek 操作，还需将其再加入到 helper 队列中，然后交换两个队列的角色；
+     * 执行 pop 操作时：与 peek 操作一样，只不过不需要将返回元素再压入 helper 栈中
      *
      */
 
