ff9d9c34cbd40c3d3a920e26c84f591c746f1632
SongNingning <qfsnn1117@gmail.com>
2020-07-26 20:23:51 +0800

    完善代码

--- 07-1 从前序与中序遍历序列构造二叉树/src/Solution.java
+++ 07-1 从前序与中序遍历序列构造二叉树/src/Solution.java
@@ -12,10 +12,17 @@ import java.util.HashMap;
  * @Date: 2020-05-16 19:23
  */
 public class Solution {
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
 
-        if (preorder.length == 0 || inorder.length == 0) {
+        if (preorder == null || inorder == null) {
             return null;
+        }
+        
+        int preLen = preorder.length;
+        int inLen = inorder.length;
+        if (preLen != inLen) {
+            throw new IllegalArgumentException("Incorrect input data.");
         }
 
         // 为了快速定位根节点在 inorder 中位置，使用 map 先存起来
@@ -24,24 +31,36 @@ public class Solution {
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+        return build(preorder, 0, preorder.length - 1, 0, inorder.length - 1, map);
     }
 
+    /**
+     * 构建二叉树
+     * @param preorder 前序遍历序列
+     * @param preL 前序遍历序列的左侧边界
+     * @param preR 前序遍历序列的右侧边界
+     * @param inL 中序遍历序列的左侧边界
+     * @param inR 中序遍历序列的右侧边界
+     * @param map 记录节点值和节点在中序遍历序列中的索引
+     * @return 构建完成的二叉树的根节点
+     */
     private TreeNode build(int[] preorder, int preL, int preR,
-                           int[] inorder, int inL, int inR,
-                           HashMap<Integer, Integer> map) {
+                           int inL, int inR, HashMap<Integer, Integer> map) {
 
         if (preL > preR || inL > inR) {
             return null;
         }
-
+        // 根节点的值
         int pivot = preorder[preL];
+        // 根节点在中序遍历序列中的位置
         int pivotIdx = map.get(pivot);
         TreeNode root = new TreeNode(pivot);
+        // 构建左子树
         root.left = build(preorder, preL + 1, preL + (pivotIdx - inL),
-                inorder, inL, pivotIdx - 1, map);
+                inL, pivotIdx - 1, map);
+        // 构建右子树
         root.right = build(preorder, preL + (pivotIdx - inL) + 1, preR,
-                inorder, pivotIdx + 1, inR, map);
+                pivotIdx + 1, inR, map);
         return root;
     }
 }
