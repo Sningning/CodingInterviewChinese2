e2e35356d360a3b4862c228cd7c519e9cb850627
SongNingning <qfsnn1117@gmail.com>
2020-07-31 16:44:08 +0800

    修改代码

--- 15-二进制中1的个数/src/Solution1.java
+++ 15-二进制中1的个数/src/Solution1.java
@@ -37,9 +37,7 @@ public class Solution1 {
 
         // int count = 0;
         // while (n != 0) {
-        //     if ((n & 1) == 1) {
-        //         count++;
-        //     }
+        //     count += (n & 1);
         //     n >>>= 1;
         // }
         // return count;
