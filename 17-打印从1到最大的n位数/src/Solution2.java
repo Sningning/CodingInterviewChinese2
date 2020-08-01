b51f094d5a0c1a1e430537fdf6e4a3ac4be9bad9
SongNingning <qfsnn1117@gmail.com>
2020-08-01 16:14:03 +0800

    添加代码

--- 17-打印从1到最大的n位数/src/Solution2.java
+++ 17-打印从1到最大的n位数/src/Solution2.java
@@ -1,22 +1,36 @@
 /**
- * 剑指offer原题
- *
- * 面试题17. 打印从1到最大的n位数
- * 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
- *
- * 示例 1:
- * 输入: n = 1
- * 输出: [1,2,3,4,5,6,7,8,9]
- *
- * @Author: Song Ningning
- * @Date: 2020-05-30 14:45
+ * @author: Song Ningning
+ * @date: 2020-08-01 11:17
  */
 public class Solution2 {
-    /*
-     * 力扣这道题给了int[]，不可能溢出，但原题要求打印
-     * 大数问题：如果 n 很大，可能会超过 int 或 long 的最大表示范围
-     */
-    public void printNumbers(int n) {
 
+    /**
+     * 也可以自己写函数实现计算幂运算
+     */
+    public int[] printNumbers(int n) {
+        int max = fastPow(10, n);
+        int[] res = new int[max - 1];
+        for (int i = 1; i < max; i++) {
+            res[i - 1] = i;
+        }
+        return res;
+    }
+
+    /**
+     * 快速幂
+     * @param base 底数
+     * @param exp 指数
+     * @return
+     */
+    private int fastPow(int base, int exp) {
+        int ans = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                ans *= base;
+            }
+            base *= base;
+            exp /= 2;
+        }
+        return ans;
     }
 }
